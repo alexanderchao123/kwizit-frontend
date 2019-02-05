6b196b59e2dfd61f1a236421de1714f5505e5773
github username <chao.alexander.123@gmail.com>
2019-02-05 14:54:45 -0500

    insert lists with Links for the NavSignedIn and NavSignedOut

---
+++ src/components/layouts/NavSignedIn.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { signOutUser } from '../../store/actions/UserActions';
+
+class NavSignedIn extends Component {
+  clickHandler = (event) => {
+    this.props.signOutUser()
+    localStorage.removeItem("token")
+  }
+
+  render() {
+    return(
+      <nav>
+        <ul>
+          <Link to="/"><li>Home</li></Link>
+          <Link to="/quizzes"><li>Explore Quizzes</li></Link>
+          <Link to="/rounds/join"><li>Join Game</li></Link>
+          <Link to="/signout"><li onClick={this.clickHandler}>Sign Out</li></Link>
+        </ul>
+      </nav>
+    )
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {signOutUser: () => dispatch(signOutUser())}
+}
+
+export default connect(null, mapDispatchToProps)(NavSignedIn)
---
+++ src/components/layouts/NavSignedOut.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class NavSignedOut extends Component {
+  render() {
+    return(
+      <nav>
+        <ul>
+          <Link to="/"><li>Home</li></Link>
+          <Link to="/quizzes"><li>Explore Quizzes</li></Link>
+          <Link to="/signup"><li>Sign Up</li></Link>
+          <Link to="/signin"><li>Sign In</li></Link>
+        </ul>
+      </nav>
+    )
+  }
+}
+
+export default NavSignedOut
