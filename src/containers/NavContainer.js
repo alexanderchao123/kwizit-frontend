a9d25927a6f975ad63c847906f75e9f0d0b8df95
github username <chao.alexander.123@gmail.com>
2019-02-05 14:53:09 -0500

    create NavSignedIn and NavSignedOut components

---
+++ src/containers/NavContainer.js
@@ -0,0 +1,27 @@
+import React, { Component, Fragment } from 'react'
+import { connect } from 'react-redux'
+import NavSignedIn from '../components/layouts/NavSignedIn'
+import NavSignedOut from '../components/layouts/NavSignedOut'
+
+class NavContainer extends Component {
+  isSignedIn = () => {
+    return !!this.props.user && this.props.isAuthenticated
+  }
+
+  render() {
+    return(
+      <Fragment>
+        {this.isSignedIn() ? <NavSignedIn/> : <NavSignedOut/>}
+      </Fragment>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    user: state.userInfo.user,
+    isAuthenticated: state.userInfo.isAuthenticated
+  }
+}
+
+export default connect(mapStateToProps)(NavContainer)
