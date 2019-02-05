7484f79beecdbbf8651a77c7a74eff1c511c44a9
github username <chao.alexander.123@gmail.com>
2019-02-05 15:00:44 -0500

    install node-sass and import NavContainer component to App component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Route, Switch, withRouter } from 'react-router-dom';
+import NavContainer from './containers/NavContainer'
 import Home from './components/layouts/Home';
 import './App.css';
 
@@ -7,6 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <NavContainer/>
         <Switch>
           <Route path="/" component={Home} />
         </Switch>
