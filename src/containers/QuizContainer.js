6228c6c46ee65cde9d1799f20366958cf2789e42
github username <chao.alexander.123@gmail.com>
2019-02-16 13:16:40 -0500

    define QuizContainer class

---
+++ src/containers/QuizContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class QuizContainer extends Component {
+  render() {
+    return(
+      <h1>QuizContainer</h1>
+    )
+  }
+}
+
+export default QuizContainer
