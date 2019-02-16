6e3465fa07529f93a723adb7bcecb42eb0d97fee
github username <chao.alexander.123@gmail.com>
2019-02-16 13:39:42 -0500

    define QuizIndex component and import it into QuizContainer

---
+++ src/components/quizzes/QuizIndex.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class QuizIndex extends Component {
+  render() {
+    return(
+      <h1>Quiz Index</h1>
+    )
+  }
+}
+
+export default QuizIndex
--- src/containers/QuizContainer.js
+++ src/containers/QuizContainer.js
@@ -1,12 +1,13 @@
 import React, { Component, Fragment } from 'react'
 import { Switch, Route} from 'react-router-dom'
+import QuizIndex from '../components/quizzes/QuizIndex'
 
 class QuizContainer extends Component {
   render() {
     return(
       <Fragment>
         <Switch>
-          
+          <Route path="/quizzes" component={QuizIndex}/>
         </Switch>
       </Fragment>
     )
