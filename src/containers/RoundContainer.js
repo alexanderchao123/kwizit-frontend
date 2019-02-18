a394f733d0af91fee6b14cd018b32117b7bebbb5
github username <chao.alexander.123@gmail.com>
2019-02-18 13:20:32 -0500

    define RoundContainer class to handle round components

---
+++ src/containers/RoundContainer.js
@@ -0,0 +1,13 @@
+import React, { Component, Fragment } from 'react'
+
+class RoundContainer extends Component {
+  render() {
+    return(
+      <Fragment>
+        <h1>RoundContainer</h1>
+      </Fragment>
+    )
+  }
+}
+
+export default RoundContainer
