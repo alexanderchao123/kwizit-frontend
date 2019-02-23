7f00a3c2c15de4c574c82e75e8dcba21d6b7ec72
github username <chao.alexander.123@gmail.com>
2019-02-23 16:13:55 -0500

    add button element to redirect to RoundScoreBoard component

--- src/components/rounds/RoundQuestionResult.js
+++ src/components/rounds/RoundQuestionResult.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react'
 class RoundQuestionResult extends Component {
   render() {
     return(
-      <h1>Round Question Result</h1>
+      <div>
+        <h1>Round Question Result</h1>
+        <button>Next</button>
+      </div>
     )
   }
 }
