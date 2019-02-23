f20b3edb8e6ae7d1ea97c96eb9ec3d6614ac1eed
github username <chao.alexander.123@gmail.com>
2019-02-23 14:45:02 -0500

    create RoundGameOver component

---
+++ src/components/rounds/RoundGameOver.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class RoundGameOver extends Component {
+  render() {
+    return(
+      <h1>Game Over</h1>
+    )
+  }
+}
+
+export default RoundGameOver
