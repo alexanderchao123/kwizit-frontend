565b529c685e13852836e11c159ae5821d4a5b63
github username <chao.alexander.123@gmail.com>
2019-02-23 20:51:26 -0500

    import RoundGameOver component into RoundContainer

--- src/containers/RoundContainer.js
+++ src/containers/RoundContainer.js
@@ -4,6 +4,7 @@ import RoundLobby from '../components/rounds/RoundLobby'
 import RoundQuestionBlock from '../components/rounds/RoundQuestionBlock'
 import RoundQuestionResult from '../components/rounds/RoundQuestionResult'
 import RoundScoreboard from '../components/rounds/RoundScoreboard'
+import RoundGameOver from '../components/rounds/RoundGameOver'
 
 class RoundContainer extends Component {
   render() {
@@ -14,6 +15,7 @@ class RoundContainer extends Component {
           <Route path="/rounds/:pin/questionblock" render={(props) => <RoundQuestionBlock {...props}/>}/>
           <Route path="/rounds/:pin/questionresult" render={(props) => <RoundQuestionResult {...props}/>}/>
           <Route path="/rounds/:pin/scoreboard" render={(props) => <RoundScoreboard {...props}/>}/>
+          <Route path="/rounds/:pin/gameover" render={(props) => <RoundGameOver {...props}/>}/>
         </Switch>
       </Fragment>
     )
