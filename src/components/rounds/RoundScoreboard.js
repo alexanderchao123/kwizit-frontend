ac8f53bcee601f2d7da580272459f455929c696e
github username <chao.alexander.123@gmail.com>
2019-02-23 20:30:36 -0500

    define RoundScoreboard component

---
+++ src/components/rounds/RoundScoreboard.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class RoundScoreboard extends Component {
+  render() {
+    return(
+      <h1>Scoreboard</h1>
+    )
+  }
+}
+
+export default RoundScoreboard
