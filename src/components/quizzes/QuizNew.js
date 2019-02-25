ccdca52362ee71e1315c87e1dfa09045b8a6473b
github username <chao.alexander.123@gmail.com>
2019-02-25 16:48:10 -0500

    add input and textarea elements for the form

--- src/components/quizzes/QuizNew.js
+++ src/components/quizzes/QuizNew.js
@@ -12,9 +12,14 @@ class QuizNew extends Component {
 
   render() {
     return(
-      <h1>
-        Quiz Form
-      </h1>
+      <div>
+        <h1>Quiz Form</h1>
+        <form>
+          <div><input type="text" name="title" placeholder="Title" value={this.state.title}/></div>
+          <div><textarea type="textarea" name="description" placeholder="Description" value={this.state.description}></textarea></div>
+          <button type="submit">Create Quiz</button>
+        </form>
+      </div>
     )
   }
 }
