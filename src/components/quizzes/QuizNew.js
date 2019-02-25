1dcb5dc0f2b4deea3a42c850ce1f0845e467f802
github username <chao.alexander.123@gmail.com>
2019-02-25 16:24:03 -0500

    create QuizNew component

---
+++ src/components/quizzes/QuizNew.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class QuizNew extends Component {
+  render() {
+    return(
+      <div>
+        Quiz Form
+      </div>
+    )
+  }
+}
+
+export default QuizNew
