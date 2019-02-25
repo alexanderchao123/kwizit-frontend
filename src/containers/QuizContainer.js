bc7c36376443172ef7a60e9c13b16ececdebbd0f
github username <chao.alexander.123@gmail.com>
2019-02-25 16:29:02 -0500

    import QuizNew component into QuizContainer and render when the url is 'quizzes/new'

--- src/containers/QuizContainer.js
+++ src/containers/QuizContainer.js
@@ -1,13 +1,15 @@
 import React, { Component, Fragment } from 'react'
 import { Switch, Route} from 'react-router-dom'
-import QuizIndex from '../components/quizzes/QuizIndex'
+import QuizNew from '../components/quizzes/QuizNew'
 import QuizShow from '../components/quizzes/QuizShow'
+import QuizIndex from '../components/quizzes/QuizIndex'
 
 class QuizContainer extends Component {
   render() {
     return(
       <Fragment>
         <Switch>
+          <Route path="/quizzes/new" render={(props) => <QuizNew {...props}/>}/>
           <Route path="/quizzes/:quizId" render={(props) => <QuizShow {...props}/>}/>
           <Route path="/quizzes" render={(props) => <QuizIndex {...props}/>}/>
         </Switch>
