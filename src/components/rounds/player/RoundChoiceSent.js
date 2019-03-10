602dca9269d5a8b80e827f1648838f1f5f21fea8
github username <chao.alexander.123@gmail.com>
2019-03-10 16:44:16 -0400

    create RoundChoiceSent component to display in a waiting period after the player sends in a choice to a specific question while other players haven't sent their answers

---
+++ src/components/rounds/player/RoundChoiceSent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class RoundChoiceSent extends Component {
+  render() {
+    return(
+      <h1>Choice Sent</h1>
+    )
+  }
+}
+
+export default RoundChoiceSent
