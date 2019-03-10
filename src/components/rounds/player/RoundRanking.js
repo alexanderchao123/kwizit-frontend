1bcdfcdd05de333b0dd115584aa66df139a5a151
github username <chao.alexander.123@gmail.com>
2019-03-10 16:46:17 -0400

    create RoundRanking component to display the player's ranking at the end of each round

---
+++ src/components/rounds/player/RoundRanking.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class RoundRanking extends Component {
+  render() {
+    return(
+      <h1>Player Ranking</h1>
+    )
+  }
+}
+
+export default RoundRanking
