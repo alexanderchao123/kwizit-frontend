ef09d380c85b7d326630f0b0d96125cd0e549cd6
github username <chao.alexander.123@gmail.com>
2019-03-16 00:30:31 -0400

    add description of Kwizit in the Home component

--- src/components/layouts/Home.js
+++ src/components/layouts/Home.js
@@ -5,6 +5,8 @@ class Home extends Component {
     return(
       <div>
         <h1>Welcome to Kwizit</h1>
+        <p>Kwizit is an interactive multiplayer quiz for teachers to utilize in a classroom setting. Users can create custom quizzes in just minutes and invite users to join a game to test other users' knowledge. There will be a user hosting the game and other users playing the game by answering questions and earning points. Join to prove how smart you are!
+        </p>
       </div>
     )
   }
